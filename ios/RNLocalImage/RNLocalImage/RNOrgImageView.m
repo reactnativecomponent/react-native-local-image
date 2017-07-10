104746515e506d96af5189b4c225e48439ec348a
guangyao <75438777@qq.com>
2017-07-10 10:12:20 +0800

    fix聊天中大图无法显示的问题

--- ios/RNLocalImage/RNLocalImage/RNOrgImageView.m
+++ ios/RNLocalImage/RNLocalImage/RNOrgImageView.m
@@ -10,10 +10,15 @@
 
 @implementation RNOrgImageView
 
+- (void)dealloc{
+    [[NSNotificationCenter defaultCenter]removeObserver:self];
+}
+
 - (instancetype)init{
     if (self = [super init]) {
         self.contentMode = UIViewContentModeScaleAspectFit;
         self.clipsToBounds = YES;
+        [self addNotificationToUpDataPic];
     }
     return self;
 }
@@ -21,12 +26,17 @@
 - (void)setPath:(NSString *)path{
     if (path) {
         _path = path;
+        UIImage *img = [[UIImage alloc]initWithContentsOfFile:_path];
+        self.image = img;
     }
 }
 
+- (void)addNotificationToUpDataPic{
+    [[NSNotificationCenter defaultCenter]addObserver:self selector:@selector(clickUpdatePicNotification) name:@"RNNeteaseimDidCompletePic" object:nil];
+}
 
-- (void)layoutSubviews{
-    if ([_path length]) {
+- (void)clickUpdatePicNotification{
+    if (!self.image) {
         UIImage *img = [[UIImage alloc]initWithContentsOfFile:_path];
         self.image = img;
     }
