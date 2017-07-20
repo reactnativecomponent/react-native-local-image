b6c6fb6bff339bdd967630a4879782dc3c3976c6
xinhui.peng <jlchome@sina.cn>
2017-07-20 15:46:50 +0800

    加载大图优化

--- android/src/main/java/com/local/image/RCTLocalImageViewManager.java
+++ android/src/main/java/com/local/image/RCTLocalImageViewManager.java
@@ -2,12 +2,17 @@ package com.local.image;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.text.TextUtils;
 import android.util.Log;
 import android.widget.ImageView;
 
 import com.facebook.react.uimanager.SimpleViewManager;
 import com.facebook.react.uimanager.ThemedReactContext;
 import com.facebook.react.uimanager.annotations.ReactProp;
+import com.local.image.media.BitmapDecoder;
+import com.local.image.media.ScreenUtil;
+
+import java.io.File;
 
 /**
  * Created by dowin on 2017/7/7.
@@ -16,6 +21,7 @@ import com.facebook.react.uimanager.annotations.ReactProp;
 public class RCTLocalImageViewManager extends SimpleViewManager<ImageView> {
 
     final static String RCT_CLASS = "RNLocalImage";
+    private BitmapFactory.Options opt = new BitmapFactory.Options();
 
     @Override
     public String getName() {
@@ -26,13 +32,18 @@ public class RCTLocalImageViewManager extends SimpleViewManager<ImageView> {
     protected ImageView createViewInstance(ThemedReactContext reactContext) {
         ImageView imageView = new ImageView(reactContext);
 //        imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+        ScreenUtil.init(reactContext);
         return imageView;
     }
 
     @ReactProp(name = "path")
     public void setPath(ImageView view, String path) {
         Log.i(RCT_CLASS, "" + path);
-        Bitmap bm = BitmapFactory.decodeFile(path);
+
+        if (TextUtils.isEmpty(path) || !new File(path).exists()) {
+            return;
+        }
+        Bitmap bm = BitmapDecoder.decodeSampledForDisplay(path);
         view.setImageBitmap(bm);
     }
 }
